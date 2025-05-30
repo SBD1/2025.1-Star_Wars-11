ac1d112ead797fc13cad38f9bc2067cfc3c3ca5e
Artur Mendonça Arruda <arturmendoncaarruda@gmail.com>
2025-05-30 20:47:36 -0300

    feat: Adiciona CASCATE em inventario_jogador e nave, adicional escopo do gitignore e requirements

--- src/inventario_jogador.sql
+++ src/inventario_jogador.sql
@@ -3,11 +3,12 @@
 -- Tabela Inventário do Jogador
 CREATE TABLE Inventario (
     Id_PlayerIn INT PRIMARY KEY,
-    Id_Player INT NOT NULL,
+    Id_Player INT NOT NULL UNIQUE,
     Espaco_Maximo INT NOT NULL,
     Peso_Total INT NOT NULL,
     FOREIGN KEY (Id_Player) REFERENCES Personagem(id_Player)
 );
+
 -- Tabela Item
 CREATE TABLE Item (
     id_item INT PRIMARY KEY,
@@ -15,13 +16,14 @@ CREATE TABLE Item (
     tipo VARCHAR(20) NOT NULL,
     preco INT
 );
+
 -- Tabela de Relacionamento entre Inventário e Item
 CREATE TABLE Inventario_Item (
     Id_PlayerIn INT,
     id_item INT,
     quantidade INT NOT NULL DEFAULT 1,
     PRIMARY KEY (Id_PlayerIn, id_item),
-    FOREIGN KEY (Id_PlayerIn) REFERENCES Inventario(Id_PlayerIn),
+    FOREIGN KEY (Id_PlayerIn) REFERENCES Inventario(Id_PlayerIn) ON DELETE CASCADE,
     FOREIGN KEY (id_item) REFERENCES Item(id_item),
     CHECK (quantidade > 0)
-); 
+);
--- src/nave.sql
+++ src/nave.sql
@@ -8,23 +8,27 @@ CREATE TABLE Nave (
     capacidade INT NOT NULL,
     FOREIGN KEY (Id_Player) REFERENCES Personagem(id_Player)
 );
+
 -- Tabela X WING T-65 (Especialização de Nave)
 CREATE TABLE X_WING_T65 (
     modelo VARCHAR(30) PRIMARY KEY,
-    FOREIGN KEY (modelo) REFERENCES Nave(modelo)
+    FOREIGN KEY (modelo) REFERENCES Nave(modelo) ON DELETE CASCADE
 );
+
 -- Tabela YT-1300 (Especialização de Nave)
 CREATE TABLE YT_1300 (
     modelo VARCHAR(30) PRIMARY KEY,
-    FOREIGN KEY (modelo) REFERENCES Nave(modelo)
+    FOREIGN KEY (modelo) REFERENCES Nave(modelo) ON DELETE CASCADE
 );
+
 -- Tabela Fregata Corelliana CR90 (Especialização de Nave)
 CREATE TABLE Fregata_Corelliana_CR90 (
     modelo VARCHAR(30) PRIMARY KEY,
-    FOREIGN KEY (modelo) REFERENCES Nave(modelo)
+    FOREIGN KEY (modelo) REFERENCES Nave(modelo) ON DELETE CASCADE
 );
+
 -- Tabela Lambda-class Shuttle (Especialização de Nave)
 CREATE TABLE Lambda_Class_Shuttle (
     modelo VARCHAR(30) PRIMARY KEY,
-    FOREIGN KEY (modelo) REFERENCES Nave(modelo)
-); 
+    FOREIGN KEY (modelo) REFERENCES Nave(modelo) ON DELETE CASCADE
+);
